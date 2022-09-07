0d2107aafc380ee83e8829cfc85e9dfdfa531811
Dragonspine <ghoul300@naver.com>
2022-09-07 23:55:33 +0900

    feat: apply message english

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.util.WebUtils;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -20,4 +23,11 @@ public class ItemServiceApplication {
 		messageSource.setDefaultEncoding("utf-8");
 		return messageSource;
 	}
+
+//	@Bean
+//	public LocaleResolver localeResolver() {
+//		CookieLocaleResolver resolver = new CookieLocaleResolver();
+//		resolver.setCookieName("lang");
+//		return resolver;
+//	}
 }
